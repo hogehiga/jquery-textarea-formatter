470c774bcc6d6673bfbc33478ac64c0d693387b7
hogehiga <hogehiga@gmail.com>
2012-08-17 01:21:52 +0900

    Add textareaformatter and unit test.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.textarea1').textareaformatter();
+    $('.textarea2').textareaformatter();
+    $('.textarea3').textareaformatter();
+    $('.textarea4').textareaformatter();
+    $('.textarea5').textareaformatter({width: 1, height: 1});
+    $('.textarea6').textareaformatter({format: false});
+});
