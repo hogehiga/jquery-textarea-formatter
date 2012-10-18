79ecb5958884824a06a6161c1213c671dc49903b
hogehiga <hogehiga@gmail.com>
2012-10-18 23:47:28 +0900

    refactoring

--- test/test.js
+++ test/test.js
@@ -5,4 +5,23 @@ $(function() {
     $('.textarea4').textareaformatter();
     $('.textarea5').textareaformatter({width: 1, height: 1});
     $('.textarea6').textareaformatter({format: false});
+
+    test('If textarea is empty, do nothing.', function() {
+        equal($('.textarea1').val(), '');
+    });
+    test('If a line has number of chars over width, that line is formatted with newline.', function() {
+        equal($('.textarea2').val(), 'ddd\nddd');
+    });
+    test('If a line has number of chars less than width, that line show as it is.', function() {
+        equal($('.textarea3').val(), 'dd\nd\ndd');
+    });
+    test('If number of lines over height, the excess rows is deleted.', function() {
+        equal($('.textarea4').val(), 'dd\ndff\ndd');
+    });
+    test('Width and height can be overridden.', function() {
+        equal($('.textarea5').val(), 'd');
+    });
+    test('You can Inhibit formatting with options override.', function() {
+        equal($('.textarea6').val(), 'abcdef');
+    });
 });
